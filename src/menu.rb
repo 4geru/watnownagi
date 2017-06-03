ccbe66ad158f7e4c439e149c5b8311d01f2cbbcd
Rp7rf <westhouse51@gmail.com>
2017-06-03 17:23:12 +0900

    fix src/menu.rb

--- src/menu.rb
+++ src/menu.rb
@@ -1,29 +1,9 @@
 require 'json'
 
-
-
 def random_menu
   menus = JSON.parse(open('./json/menus.json') { |io|
     JSON.load(io)
   })
   key = menus.keys.shuffle[0]
   menus[key]
-end
-
-def reply_menu
-  menu = random_menu
-  {
-    "type": "template",
-    "altText": "給食ガチャ",
-    "template": {
-        "type": "carousel",
-        "columns": [
-            {
-              "thumbnailImageUrl": "https://www.u-coop.net/food/menu/menu_images/#{menu['id']}.jpg",
-              "title": "#{menu['title']}",
-            }
-        ]
-
-    }
-  }
 end
